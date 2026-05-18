fc104c420cd15490bbc20b09f4c670fc07c00257
Binseaaa <vinsmar8888@gmail.com>
2026-05-18 21:45:16 +0800

    deleted images and changed to cloudinary images for thumbnail

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -11,7 +11,7 @@ export const TIKTOK_VIDEOS: VideoItem[] = [
   {
     id: '4mode-faucet',
     title: '4Mode Faucet',
-    img: 'ASSETS/TikTok/4Mode Faucet.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/4Mode_Faucet_m2x5wg.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/4Mode_Faucet_wtc6c2.mp4',
     aspect: '9/16',
     category: 'tiktok',
@@ -19,7 +19,7 @@ export const TIKTOK_VIDEOS: VideoItem[] = [
   {
     id: 'mini-shaver',
     title: 'Mini Shaver',
-    img: 'ASSETS/TikTok/Mini Shaver.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Mini_Shaver_ahz0zt.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Mini_Shaver_mftsiw.mp4',
     aspect: '9/16',
     category: 'tiktok',
@@ -27,7 +27,7 @@ export const TIKTOK_VIDEOS: VideoItem[] = [
   {
     id: 'ripsurf',
     title: 'RipSurf Skateboard',
-    img: 'ASSETS/TikTok/surfing.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/surfing_nblxz5.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/RipSurf_Skateboard_-_101_mtnk7r.mp4',
     aspect: '9/16',
     category: 'tiktok',
@@ -35,7 +35,7 @@ export const TIKTOK_VIDEOS: VideoItem[] = [
   {
     id: 'mosquito-bracelet',
     title: 'Mosquito Bracelet',
-    img: 'ASSETS/TikTok/Mosquito Bracelet .png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Mosquito_Bracelet_b9gyb2.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Mosquito_Bracelet_kjvqum.mp4',
     aspect: '9/16',
     category: 'tiktok',
@@ -43,7 +43,7 @@ export const TIKTOK_VIDEOS: VideoItem[] = [
   {
     id: 'waterproof-phone',
     title: 'Waterproof Phone Holder',
-    img: 'ASSETS/TikTok/Water Proof PhoneHolder.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Water_Proof_PhoneHolder_csmtmt.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Waterproof_Phoneholder_dzuzka.mp4',
     aspect: '9/16',
     category: 'tiktok',
@@ -51,7 +51,7 @@ export const TIKTOK_VIDEOS: VideoItem[] = [
   {
     id: 'facemask-machine',
     title: 'Wholefacemask Machine',
-    img: 'ASSETS/TikTok/Wholefacemask Maker Machine.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Wholefacemask_Maker_Machine_n5xr7o.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Wholefacemask_machine_zwco4l.mp4',
     aspect: '9/16',
     category: 'tiktok',
@@ -59,7 +59,7 @@ export const TIKTOK_VIDEOS: VideoItem[] = [
   {
     id: 'shoe-cleaner',
     title: 'Shoe Cleaner',
-    img: 'ASSETS/TikTok/Shoe Cleaner.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Shoe_Cleaner_ipmecv.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Shoe_Cleaner_9x16_ur1zvu.mp4',
     aspect: '9/16',
     category: 'tiktok',
@@ -70,7 +70,7 @@ export const META_VIDEOS: VideoItem[] = [
   {
     id: 'intense-pulsed-light',
     title: 'Intense Pulsed Light',
-    img: 'ASSETS/Meta Ads/Intense Pulsed Light - 101.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Intense_Pulsed_Light_-_101_pga9ff.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Intense_Pulsed_Light_-_101_2x_byh5yg.mp4',
     aspect: '1/1',
     category: 'meta',
@@ -78,7 +78,7 @@ export const META_VIDEOS: VideoItem[] = [
   {
     id: 'robot',
     title: 'Robot',
-    img: 'ASSETS/Meta Ads/Robot - 101.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Robot_-_101_e6z5jo.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Robot_-_101_owwzv7.mp4',
     aspect: '1/1',
     category: 'meta',
@@ -86,7 +86,7 @@ export const META_VIDEOS: VideoItem[] = [
   {
     id: 'triple-monitor',
     title: 'Triple Monitor',
-    img: 'ASSETS/Meta Ads/Triple Monitor - 101.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Triple_Monitor_-_101_fpjv8x.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Triple_Monitor_-_101_exbx5e.mp4',
     aspect: '1/1',
     category: 'meta',
@@ -94,7 +94,7 @@ export const META_VIDEOS: VideoItem[] = [
   {
     id: 'a-maze-ball',
     title: 'A-Maze Ball',
-    img: 'ASSETS/Meta Ads/A Maze Ball.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/A_Maze_Ball_i7cln0.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/A-Maze_Ball_gjcxlq.mp4',
     aspect: '1/1',
     category: 'meta',
@@ -102,7 +102,7 @@ export const META_VIDEOS: VideoItem[] = [
   {
     id: 'lint-remover',
     title: 'Lint Remover',
-    img: 'ASSETS/Meta Ads/Lint Remover.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Lint_Remover_iopwqu.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Lint_Remover_wp3zce.mp4',
     aspect: '1/1',
     category: 'meta',
@@ -110,7 +110,7 @@ export const META_VIDEOS: VideoItem[] = [
   {
     id: 'sunscreen-applicator',
     title: 'Sunscreen Applicator',
-    img: 'ASSETS/Meta Ads/Sunscreen Applicator.png',
+    img: 'https://res.cloudinary.com/drmpnuh83/image/upload/Sunscreen_Applicator_ag5q8z.png',
     src: 'https://res.cloudinary.com/drmpnuh83/video/upload/Sunscreen_Applicator_teqhiu.mp4',
     aspect: '1/1',
     category: 'meta',
